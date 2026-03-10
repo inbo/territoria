284d14d65b5fa5875b3ac188ad426ca7b12492cf
CARMEN <raisa.carmen@inbo.be>
2026-03-10 13:59:36 +0100

    Add user to edges

--- R/edge_distribution.R
+++ R/edge_distribution.R
@@ -13,15 +13,17 @@ edge_distribution <- function(conn, max_dist = 336) {
     inherits(conn, "SQLiteConnection"),
     is.number(max_dist), noNA(max_dist), max_dist > 0
   )
-  "SELECT survey, x, y FROM observation" |>
+  "SELECT survey, user, x, y FROM observation" |>
     dbGetQuery(conn = conn) -> observations
   table(observations$survey) |>
     as.data.frame() -> n_survey
   n_survey[n_survey$Freq > 1, ] |>
     merge(x = observations, by.y = "Var1", by.x = "survey") -> observations
-  edges <- data.frame(survey = integer(0), length = numeric(0))
+  edges <- data.frame(survey = integer(0), user = integer(0),
+                      length = numeric(0))
   for (i in unique(observations$survey)) {
     candidate <- which(observations$survey == i)
+    user <- observations[candidate[1], "user"]
     dd <- deldir(
       x = rnorm(length(candidate), mean = observations$x[candidate], sd = 0.01),
       y = rnorm(length(candidate), mean = observations$y[candidate], sd = 0.01),
@@ -29,6 +31,7 @@ edge_distribution <- function(conn, max_dist = 336) {
     )
     extra <- data.frame(
       survey = i,
+      user = user,
       length = sqrt(
         (dd$delsgs$x2 - dd$delsgs$x1) ^ 2 + (dd$delsgs$y2 - dd$delsgs$y1) ^ 2
       )
